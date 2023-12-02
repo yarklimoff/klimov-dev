016d6a472c8bb42cbe5f644cf876e95b3d2e1917
Klimov Yaroslav <klimov.iav@phystech.edu>
2023-12-02 17:17:29 +0300

    init

--- src/components/charts/lineChart/Curves/Curves.tsx
+++ src/components/charts/lineChart/Curves/Curves.tsx
@@ -9,20 +9,7 @@ const Curves = observer(() => {
   const model = useD3Context();
   return (
     <>
-      <clipPath id={`clipPath`}>
-        <rect
-          x={model.padding.left + model.leftAxisCount * 65 - 5}
-          y={model.padding.top}
-          width={
-            model.width -
-            model.padding.left -
-            model.padding.right -
-            model.leftAxisCount * 65 -
-            model.rightAxisCount * 65 +
-            10
-          }
-          height={model.height - model.padding.top - model.padding.bottom}></rect>
-      </clipPath>
+      
       {model.curves.map((_, i) => (
         <Curve i={i} key={i}></Curve>
       ))}
